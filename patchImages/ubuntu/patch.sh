40d077b125b4b7f03357c4fc6304bd908d7771eb
Dustin <dustinh@onesi.de>
2023-12-09 12:25:31 +0100

    fix patch script

--- patchImages/ubuntu/patch.sh
+++ patchImages/ubuntu/patch.sh
@@ -2,12 +2,12 @@
 echo run as
 id
 echo deleting existing sources
-rm /host/etc/apt/sources.list.d/*.list
-rm /host/etc/apt/sources.list
+rm -f /host/etc/apt/sources.list.d/*.list
+rm -f /host/etc/apt/sources.list
 
 echo update apt repositories
-cp /patch/sources/ubuntu.list /host/etc/apt/sources.list
-cp /patch/sources/crio.list   /host/etc/apt/sources.list.d/crio.list
+cp /patch/source/ubuntu.list /host/etc/apt/sources.list
+cp /patch/source/crio.list   /host/etc/apt/sources.list.d/crio.list
 cp -r /patch/keyrings         /host/usr/share/keyrings
 
 chroot /host apt-get update
