98574d18c1b84fdbfb0ea92b575dcdf9d1138987
Dustin <dustinh@onesi.de>
2023-12-10 09:50:41 +0100

    install and hold specified packages

--- patchImages/ubuntu/patch.sh
+++ patchImages/ubuntu/patch.sh
@@ -11,6 +11,18 @@ cp /patch/source/ubuntu.list /host/etc/apt/sources.list
 cp /patch/source/crio.list   /host/etc/apt/sources.list.d/crio.list
 cp -r /patch/keyrings         /host/usr/share/keyrings
 
+echo fetch new updates from repositories
 chroot /host su - root -c 'apt-get update'
 
+if [ -n "${HOLDPKG}" ]
+then
+  echo hold specified packages: ${HOLDPKG}
+  chroot /host su - root -c "apt-mark hold ${HOLDPKG}"
+fi
+
+if [ -n "${INSTALLPKG}" ]
+then
+  echo force install specified packages: ${INSTALLPKG}
+  chroot /host su - root -c "apt-get install ${INSTALLPKG}"
+fi
 
